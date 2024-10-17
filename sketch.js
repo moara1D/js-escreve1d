708c8be7acfd093738d521ccf2858bef6ba37a8f
moara1D <00001140006058sp@al.educacao.sp.gov.br>
2024-10-17 16:55:03 -0300

    js e css

---
+++ sketch.js
@@ -0,0 +1,18 @@
+let xBolinha = 300;
+let yBolinha = 200;
+let diametro = 15;
+
+let velocidadexBolinha = 6;
+let velocidadeyBolinha = 6;
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0);
+  circle(xBolinha, yBolinha, diametro);
+  xBolinha = xBolinha +1;
+  yBolinha = yBolinha +1;
+  
+  if()
+}
